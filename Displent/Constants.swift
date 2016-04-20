dcbc3cdf5a67d2a06a4e2c4d5c9f3f09bb4e6d07
srikantviswanath <srikantviswanath8@gmail.com>
2016-04-19 20:24:34 -0400

    Initial commit with initial UI for login screen

---
+++ Displent/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Displent
+//
+//  Created by Srikant Viswanath on 4/19/16.
+//  Copyright © 2016 Srikant Viswanath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
---
+++ Displent/MaterialView.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialView.swift
+//  Displent
+//
+//  Created by Srikant Viswanath on 4/19/16.
+//  Copyright © 2016 Srikant Viswanath. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 3.5
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
