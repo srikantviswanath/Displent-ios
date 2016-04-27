36a99ee90c2cd829c83067c03de1bc2e087a864a
srikantviswanath <srikantviswanath8@gmail.com>
2016-04-26 21:13:05 -0400

    Created initial /Model/Post.swift and connecting to Firebase
    1. Implemented initial code to talk to Firebase and parse the dictionary
    2. Created initial Post.swift file with initial instance variables and
    2 constructors

--- Displent/HomeFeedVC.swift
+++ Displent/HomeFeedVC.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import Firebase
+
 
 class HomeFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var postsList = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        DataService.dataService.REF_POSTS.observeEventType(.Value, withBlock:{ snapshot in
+            self.postsList = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot]{
+                for snap in snapshots {
+                    if let postDict = snap.value as? Dictionary<String, AnyObject>{
+                        let postKey = snap.key
+                        let post = Post(postKey: postKey, postDict: postDict)
+                        self.postsList.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -23,10 +40,12 @@ class HomeFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return postsList.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = postsList[indexPath.row]
+        print(post.postDescription)
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 }
---
+++ Displent/Post.swift
@@ -0,0 +1,48 @@
+//
+//  Post.swift
+//  Displent
+//
+//  Created by Srikant Viswanath on 4/26/16.
+//  Copyright © 2016 Srikant Viswanath. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String{
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var username: String{
+        return _username
+    }
+    
+    var postKey: String{
+        return _postKey
+    }
+    
+    /*init for a shell object before writing to Firebase. Hence doesnt have postKey*/
+    init(description: String, imgUrl: String?, username: String){
+        self._postDescription = description
+        self._imageUrl = imgUrl
+        self._username = username
+    }
+    
+    /*init to create an instance of Post after grabbing Firebase post object and parsing the dictionary*/
+    init(postKey: String, postDict: Dictionary<String, AnyObject>){
+        self._postKey = postKey
+        if let likes = postDict["likes"] as? Int {self._likes = likes}
+        if let imgUrl = postDict["imgUrl"] as? String {self._imageUrl = imgUrl}
+        if let postDesc = postDict["description"] as? String {self._postDescription = postDesc}
+    }
+}
